1093481baad90cfbf457f27c207166f18a3296c0
ronaldowa <rwatecnologia@gmail.com>
2026-06-27 01:32:11 -0300

    bug main

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,9 @@ import { NestFactory } from '@nestjs/core';
 
 import { ValidationPipe } from '@nestjs/common';
 
-import * as cookieParser from 'cookie-parser';
-
-
+import cookieParser from 'cookie-parser';
 
 import { AppModule } from './app.module';
-
 
 
 async function bootstrap() {
@@ -17,12 +14,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
 
-
-
-
   app.use(cookieParser());
-
-
 
   app.enableCors({
 

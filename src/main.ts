0b605889fc2485a4e556c3c8365d9728f7354cf2
RONALDO WILSON DE AGUIAR <43527497+ronaldowa@users.noreply.github.com>
2026-07-09 19:32:16 -0300

    Add ngrok URL to allowed origins

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,8 @@ async function bootstrap() {
     origin: [
 
       'https://inclusivetech.vercel.app',
-      'http://localhost:5173'
+      'http://localhost:5173',
+      'https://dill-marlin-snowbird.ngrok-free.dev'
 
     ],
 
